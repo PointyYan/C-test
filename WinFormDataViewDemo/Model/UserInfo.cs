7b1c7daebf722181e7d8b775195d6f301e2672d0
Yan <yjq97@outlook.com>
2017-12-13 22:44:24 +0800

    add a pro about dataview

---
+++ WinFormDataViewDemo/Model/UserInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class UserInfo
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public UserInfo() { }
+
+        public UserInfo(int ID, string UserName, string Password)
+        {
+            this.ID = ID;
+            this.UserName = UserName;
+            this.Password = Password;
+            //this.Email = Email;
+        }
+    }
+}
---
+++ WinFormDataViewDemo/WinFormDataViewDemo/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Model;
+using WinFormDataViewDemo;
+
+namespace WinFormDataViewDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAllUser_Click(object sender, EventArgs e)
+        {
+            string sql = "select * from UserInfo";
+            //DataSet dataSet = new DataSet();
+            //dataSet = DBHelper.ExecuteDataSet(sql);
+            //dataGridView1.DataSource = dataSet.Tables[0];
+            DataTable dataTable = new DataTable();
+            dataTable = DBHelper.ExecuteDataTable(sql);
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
